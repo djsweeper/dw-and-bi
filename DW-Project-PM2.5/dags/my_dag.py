7720e0efa2ee65fcfb8f96be9d8c5c5a4d411c35
djsweeper <95906150+djsweeper@users.noreply.github.com>
2024-05-03 05:16:06 +0000

    add new project

--- DW-Project-PM2.5/dags/my_dag.py
+++ DW-Project-PM2.5/dags/my_dag.py
@@ -30,13 +30,32 @@ def _get_files():
         names = link.split("/")[-1]
         names = names.strip()
         name = names.replace("pm_data_hourly-","")
-        req = requests.get(link, verify=False)
-        url_content = req.content
-        file_p = "/opt/airflow/dags/" + name
-        csv_file = open(name, "wb")
-        csv_file.write(url_content)
-        csv_file.close()
-    print(os.listdir("/opt/airflow/dags/"))
+        if name != "data_dictionary.csv":
+            req = requests.get(link, verify=False)
+            url_content = req.content
+            file_p = "/opt/airflow/dags/data/" + name
+            csv_file = open(file_p, "wb")
+            csv_file.write(url_content)
+            csv_file.close()
+
+# def _upload_to_gcs(data_folder,gcs_path,**kwargs):
+#     data_folder = "/opt/airflow/dags/data/" # local dir
+#     bucket_name = "swu-ds-525-525" # bucket name on GCS
+#     gcs_conn_id = "my_scp_conn"
+#     csv_files = [file for file in os.listdir(data_folder) if file.endswith(".csv")]
+#     for csv_file in csv_files:
+#         local_file_path = os.path.join(data_folder, csv_file)
+#         gcs_file_path = f"{gcs_path}/{csv_file}"
+
+#         upload_task = LocalFilesystemToGCSOperator(
+#             task_id = f"upload_to_gcs",
+#             src = local_file_path,
+#             dst = gcs_file_path,
+#             bucket = bucket_name,
+#             gcp_conn_id = "my_scp_conn"
+#         )
+#         upload_task.execute(content=kwargs)
+
 
 with DAG(
     "sample",
@@ -54,20 +73,27 @@ with DAG(
         task_id = "get_files",
         python_callable = _get_files,
     )
+    data_folder = "/opt/airflow/dags/data/" # local dir
+    gcs_path = "gs://swu-ds-525-525/"
+    bucket_name = "swu-ds-525-525" # bucket name on GCS
+    csv_files = [file for file in os.listdir(data_folder) if file.endswith(".csv")]
+    path = []
+    for csv_file in csv_files:
+        path.append(data_folder + csv_file)
     
-    # upload_gcs = LocalFilesystemToGCSOperator(
-    #     task_id = "upload_task",
-    #     src = "/opt/airflow/dags/",
-    #     dst = "gs://swu",
-    #     bucket_name = "swu-ds-525-525",
-    #     move_object=True,
-    #     gcp_conn_id = "my_gcp_conn",
-    #     mime_type = "text/csv"
-    # )
+    upload_to_gcs = LocalFilesystemToGCSOperator(
+        task_id = "upload_to_gcs",
+        src = path,
+        dst = gcs_path,
+        bucket = bucket_name,
+        gcp_conn_id = "my_scp_conn",
+        )      
+
+    
 
     end = EmptyOperator(
         task_id="end",
     )
 
     
-    start >> get_files >> end
+    start >> get_files >> upload_to_gcs >> end
